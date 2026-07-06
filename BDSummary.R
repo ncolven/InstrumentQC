2554b04ce1120e5c3593f6b2bb19b644228c573c
ncolven <ncolven@fredhutch.org>
2026-07-06 15:54:56 -0700

    Script updates

--- BDSummary.R
+++ BDSummary.R
@@ -1,17 +1,31 @@
 BDSummary <- function (x, Instrument, detectorType = "-A"){
   MFICheck <- function(x){
-    for (i in 1:nrow(x)){
-      for (j in 1:ncol(x)){
+    data = x
+    for (j in 1:ncol(x)){
+      for (i in 1:nrow(x)){
         if (x[i,j] <=7999){
-          x[i,j] <- "Red"
-        }else if (x[i,j] >=12001){
-          x[i,j] <- "Orange"
+          data[i,j] <- "Red"
+        }else if (x[i,j] >12000){
+          data[i,j] <- "Orange"
         }else{
-          x[i,j] <- "Green"
+          data[i,j] <- "Green"
         }
       }
     }
-    return(x)
+    return(data)
+  }
+  RCVCheck <- function(x){
+    data <- x
+    for (i in 1:nrow(x)){
+      for (j in 1:ncol(x)){
+        if (x[i,j] >=15){
+          data[i,j] <- "Red"
+        } else {
+          data[i,j] <- "Green"
+        }
+      }
+    }
+    return(data)
   }
   WindowOfInterest <- Sys.time() - weeks(1)
   if (nrow(x) > 1) {
@@ -27,6 +41,7 @@ BDSummary <- function (x, Instrument, detectorType = "-A"){
   TheColumns <- setdiff(TheColumns, "TIME")
   
   # MFIs
+  {
   TheIntermediate <- TheColumns[!str_detect(TheColumns, "Gain")]
   TheIntermediate <- TheIntermediate[!str_detect(TheIntermediate, "rCV")]
   TheColumnNames <- TheIntermediate[str_detect(TheIntermediate, "-A")]
@@ -49,4 +64,40 @@ BDSummary <- function (x, Instrument, detectorType = "-A"){
   BlueStatus <- MFICheck(BlueMFI)
   YellowGreenStatus <- MFICheck(YellowGreenMFI)
   RedStatus <- MFICheck(RedMFI)
+  
+  InstrumentMFI <- relocate(mutate(bind_cols(c(UVMFI,VioletMFI,BlueMFI,YellowGreenMFI,RedMFI)), 
+                  DateTime = Data$DateTime), DateTime, .before = 1)
+  MFIStatus <- relocate(mutate(bind_cols(c(UVStatus,VioletStatus,BlueStatus,YellowGreenStatus,RedStatus)), 
+                      DateTime = Data$DateTime), DateTime, .before = 1)
+  }
+  # rCVs
+  {
+    TheIntermediate <- TheColumns[str_detect(TheColumns, "rCV")]
+    TheColumnNames <- TheIntermediate[str_detect(TheIntermediate, "-A")]
+    
+    if (Instrument == "Fortessa"){
+      UVNames <- TheColumnNames[16:22]  
+      VioletNames <- TheColumnNames[23:30]
+      BlueNames <- TheColumnNames[3:7]
+      YellowGreenNames <- TheColumnNames[8:12]
+      YellowGreenNames[length(YellowGreenNames)+1] <- TheColumnNames[31]
+      RedNames <- TheColumnNames[13:15]
+      
+      UVRCV <- Data[,UVNames]
+      VioletRCV <- Data[,VioletNames]
+      BlueRCV <- Data[,BlueNames]
+      YellowGreenRCV <- Data[,YellowGreenNames]
+      RedRCV <- Data[,RedNames]
+    }
+    UVStatus <- RCVCheck(UVRCV)
+    VioletStatus <- RCVCheck(VioletRCV)
+    BlueStatus <- RCVCheck(BlueRCV)
+    YellowGreenStatus <- RCVCheck(YellowGreenRCV)
+    RedStatus <- RCVCheck(RedRCV)
+    
+    RCVStatus <- bind_cols(c(UVStatus,VioletStatus,BlueStatus,YellowGreenStatus,RedStatus))
+  }
+  InstrumentStatus <- bind_cols(c(MFIStatus, RCVStatus))
+  #Use tidyr::pivot_longer to convert from wide to long data
+  return(InstrumentStatus)
 }
