874c2d8d96fa8092458afa7ac984ac352644239d
ncolven <ncolven@fredhutch.org>
2026-07-21 15:52:37 -0700

    DailyTable edits

--- scrapnotes.R
+++ scrapnotes.R
@@ -2,13 +2,17 @@ test <- pivot_wider(InstrumentStatus, names_from = Detector, values_from = c(MFI
 test <- test[!str_detect(colnames(test), "Status")]
 slice_head(test)
 
-
-DetailTable <- arrange(InstrumentStatus, desc(InstrumentStatus$DateTime))
+IntermediateTable <- tidyr::pivot_longer(InstrumentStatus[,!str_detect(colnames(InstrumentStatus), "rCV")], !DateTime, names_to="Detector", values_to="MFI")
+RCVTable <- pivot_longer(mutate(DateTime = InstrumentStatus$DateTime, InstrumentStatus[,str_detect(colnames(InstrumentStatus), "rCV")]),
+                         !DateTime, values_to = "RCV")
+IntermediateTable <- bind_cols(IntermediateTable, RCVTable[,3])
+DetailTable <- arrange(IntermediateTable, desc(IntermediateTable$DateTime))
 DetailTable <- DetailTable[str_detect(as_date(DetailTable$DateTime), 
                                       as.character(as_date(DetailTable$DateTime[1]))),]
+TableDate <- as.character(as_date(DetailTable$DateTime[1]))
 DetailTable <- DetailTable[,-1]
-
-table <- DetailTable %>% gt() %>% data_color(columns = MFI, 
+DailyTable <- DetailTable %>% gt() %>% tab_header(title = TableDate) %>%
+                                      data_color(columns = MFI, 
                                       fn = function(x) {
                                         dplyr::case_when(between(x, 8000, 12000) ~ "#0B6623", x > 12000 ~ 
                                                            "#FF6E00", x < 8000 ~ 
@@ -18,7 +22,7 @@ table <- DetailTable %>% gt() %>% data_color(columns = MFI,
                                                           dplyr::case_when(x < 15 ~ "#0B6623", x >= 15 ~ 
                                                                              "#C80815", TRUE ~ NA_character_)
                                                         })
-table
+DailyTable
 
 
 InstrumentMFI <- tidyr::pivot_longer(InstrumentMFI, !DateTime, names_to = "Detector", values_to = "MFI")
