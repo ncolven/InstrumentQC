6fcbbaba53eed6c2ddc8b279994c38764200015b
ncolven <ncolven@fredhutch.org>
2026-08-21 10:56:12 -0700

    Added creds instructions

--- Package installer.R
+++ Package installer.R
@@ -12,3 +12,9 @@ BiocManager::install(c("flowCore",
                        "ggcyto"))
 install_github("https://github.com/DavidRach/Luciernaga",
                dependencies = TRUE)
+
+## Generate new github token: https://github.com/settings/tokens
+gitcreds::gitcreds_set()
+
+## Generate ssh key: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+## Add key to github: https://github.com/settings/keys
