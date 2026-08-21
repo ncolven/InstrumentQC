25d2ad498b0b4bdb19e4c7d593a4226df72ef80a
ncolven <ncolven@fredhutch.org>
2026-08-21 12:47:53 -0700

    Update for Symphony-3 on 2026-08-21

--- Package installer.R
+++ Package installer.R
@@ -5,16 +5,16 @@ install.packages(c("devtools",
                    "pandoc", 
                    "ggbeeswarm",
                    "git2r"))
-library(devtools)
-library(BiocManager)
+
 BiocManager::install(c("flowCore",
                        "flowWorkspace",
                        "ggcyto"))
-install_github("https://github.com/DavidRach/Luciernaga",
+devtools::install_github("https://github.com/DavidRach/Luciernaga",
                dependencies = TRUE)
 
 ## Generate new github token: https://github.com/settings/tokens
 gitcreds::gitcreds_set()
+usethis::edit_r_environ()
 
 ## Generate ssh key: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 ## Add key to github: https://github.com/settings/keys
